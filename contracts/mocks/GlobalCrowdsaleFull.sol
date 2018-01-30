047e119e7f5c009047cdd8d3432610eb62bfa06d
Markus Thomsen <matho@itu.dk>
2018-01-30 07:35:55 +0100

    Update values of full crowdsale

--- contracts/mocks/GlobalCrowdsaleFull.sol
+++ contracts/mocks/GlobalCrowdsaleFull.sol
@@ -9,14 +9,14 @@ contract GlobalCrowdsaleFull is GlobalCrowdsaleImpl {
     GlobalCrowdsaleImpl(
         1517500800, //Start epoch time, should equal February 1st
         1520006400, //End epoch time, should equal March 1st
-        11000, //Wei Rate, amount of tokens per eth 
+        12000, //Wei Rate, amount of tokens per eth 
         address(0x244B9b943C6f06206f1E6Eef537Ba000c48fFDED), //Address of the receiving wallet
-        9090909090909090909090, //Maximum amount of wei that can be received
-        4545454545454545454545, // Minimum amount of wei for successful crowdsale
+        8333.3333333333333333 ether, //Maximum amount of wei that can be received
+        4166.6666666666666666 ether, // Minimum amount of wei for successful crowdsale
         1520006400, // Date of which tokens can be transfered freely
-        10 ether, // Maximum investment denominated in ether
-        0.045 ether, // Minimum investment denominated in ether
-        11000*9090909090909090909090*3) // Total amount of token bits to be created
+        8.33 ether, // Maximum investment denominated in ether
+        0.04 ether, // Minimum investment denominated in ether
+        12000*(8333.3333333333333333 ether)*3) // Total amount of token bits to be created
   {
   }
 }
